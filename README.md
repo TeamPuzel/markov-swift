a331197ef74b5bf8b6ff5d794952287a6e2dd77a
TeamPuzel <puffed-01.blips@icloud.com>
2022-09-21 12:13:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# markov-swift
+
+A description of this package.
---
+++ Tests/markov-swiftTests/markov_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import markov_swift
+
+final class markov_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(markov_swift().text, "Hello, World!")
+    }
+}
